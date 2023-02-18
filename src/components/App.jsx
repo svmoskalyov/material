673d9334862b00ecddb08169fd1c0b868073448f
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-18 09:54:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,35 @@
 import { Component } from 'react';
 import { Box } from './Box';
+import * as API from 'services/api';
+import { MaterialEditorForm } from './MaterialEditorForm/MaterialEditorForm';
+import { MaterialList } from './MaterialList/MaterialList';
 
 export class App extends Component {
-  state = {};
+  state = {
+    materials: [],
+    isLoading: false,
+  };
+
+  addMaterial = async values => {
+    try {
+      this.setState({ isLoading: true });
+      const material = await API.addMaterial(values);
+      this.setState(state => ({
+        materials: [...state.materials, material],
+        isLoading: false,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   render() {
+    const { materials } = this.state;
+
     return (
       <Box width="500px" mx="auto" p={4}>
-        APP
+        <MaterialEditorForm onSubmit={this.addMaterial} />
+        <MaterialList items={materials} />
       </Box>
     );
   }
