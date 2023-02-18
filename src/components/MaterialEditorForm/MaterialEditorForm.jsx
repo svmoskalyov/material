a314b63d05cdb7fb56af8017b1b36ed9d950ab1d
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-18 09:54:42 +0200

    Create MaterialEditorForm.jsx

---
+++ src/components/MaterialEditorForm/MaterialEditorForm.jsx
@@ -0,0 +1,32 @@
+import { Field, Form, Formik } from 'formik';
+
+export const MaterialEditorForm = ({ onSubmit }) => {
+  const handleSubmit = async (values, actions) => {
+    await onSubmit(values);
+    actions.setSubmitting(false);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik initialValues={{ title: '', link: '' }} onSubmit={handleSubmit}>
+      {({ isSubmitting }) => (
+        <Form>
+          <label htmlFor="">
+            Description
+            <Field name="title" type="text" />
+          </label>
+          <br />
+          <label htmlFor="">
+            URL
+            <Field name="link" type="text" />
+            {/* <Field name="link" type="url" /> */}
+          </label>
+          <br />
+          <button type="submit" disabled={isSubmitting}>
+            Add material
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
