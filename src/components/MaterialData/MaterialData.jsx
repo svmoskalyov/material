af3ca8dd3e085a322d90911ea3ba5482defd2591
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-21 16:58:18 +0200

    Create MaterialData.jsx

---
+++ src/components/MaterialData/MaterialData.jsx
@@ -0,0 +1,154 @@
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+import { AiOutlineCloudDownload } from 'react-icons/ai';
+import { Section } from 'components/Section/Section';
+import { Loader } from 'components/Loader/Loader';
+import { Button } from 'components/Button/Button';
+import { MaterialError } from 'components/MaterialError/MaterialError';
+import { MaterialForm } from 'components/MaterialForm/MaterialForm';
+import { MaterialList } from 'components/MaterialList/MaterialList';
+import * as API from 'services/api';
+
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  REJECTED: 'rejected',
+  RESOLVED: 'resolved',
+};
+
+export class MaterialData extends Component {
+  state = {
+    materials: [],
+    error: null,
+    status: Status.IDLE,
+  };
+
+  //   async componentDidMount() {
+  //     try {
+  //       this.setState({ status: Status.PENDING });
+  //       const materials = await API.getMaterials();
+  //       this.setState({ materials, status: Status.RESOLVED });
+  //     } catch (error) {
+  //       this.setState({ error, status: Status.REJECTED });
+  //       console.log(error);
+  //     }
+  //   }
+
+  getMaterial = async () => {
+    try {
+      this.setState({ status: Status.PENDING });
+      const materials = await API.getMaterials();
+      this.setState({ materials, status: Status.RESOLVED });
+    } catch (error) {
+      this.setState({ error, status: Status.REJECTED });
+      toast.error(error.message);
+    }
+  };
+
+  addMaterial = async values => {
+    try {
+      const material = await API.addMaterial(values);
+      this.setState(state => ({
+        materials: [...state.materials, material],
+      }));
+    } catch (error) {
+      this.setState({ error, status: Status.REJECTED });
+      toast.error(error.message);
+    }
+  };
+
+  deleteMaterial = async id => {
+    try {
+      await API.deleteMaterial(id);
+      this.setState(state => ({
+        materials: state.materials.filter(material => material.id !== id),
+      }));
+    } catch (error) {
+      this.setState({ error, status: Status.REJECTED });
+      toast.error(error.message);
+    }
+  };
+
+  updateMaterial = async fields => {
+    try {
+      const updatedMaterial = await API.updateMaterial(fields);
+      this.setState(state => ({
+        materials: state.materials.map(material =>
+          material.id === fields.id ? updatedMaterial : material
+        ),
+      }));
+    } catch (error) {
+      this.setState({ error, status: Status.REJECTED });
+      toast.error(error.message);
+    }
+  };
+
+  render() {
+    const { materials, error, status } = this.state;
+
+    if (status === 'idle') {
+      return (
+        <Section title="ADD MATERIAL">
+          <MaterialForm onSubmit={this.addMaterial} />
+          <Button
+            type="button"
+            icon={AiOutlineCloudDownload}
+            onClick={this.getMaterial}
+          >
+            Dowload list materials...
+          </Button>
+        </Section>
+      );
+    }
+
+    if (status === 'pending') {
+      return <Loader />;
+    }
+
+    if (status === 'rejected') {
+      return <MaterialError message={error.message} />;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <Section title="LIST MATERIALS">
+          <MaterialList
+            items={materials}
+            onDelete={this.deleteMaterial}
+            onUpdate={this.updateMaterial}
+          />
+        </Section>
+      );
+    }
+
+    // return (
+    //   <>
+    //     <Section title="ADD MATERIAL">
+    //       {/* <MaterialEditorForm onSubmit={this.addMaterial} /> */}
+    //     </Section>
+    //     <Section title="LIST MATERIALS">
+    //       {/* <MaterialList
+    //       items={materials}
+    //       onDelete={this.deleteMaterial}
+    //       onUpdate={this.updateMaterial}
+    //     /> */}
+    //     </Section>
+    //   </>
+    // );
+
+    //   const { materials, isLoading, error } = this.state;
+    //         {error && (
+    //           <p>Oh! Something went wrong. Reload the page and try again time.</p>
+    //         )}
+    //           <MaterialEditorForm onSubmit={this.addMaterial} />
+    //         {isLoading ? (
+    //           <Loader />
+    //         ) : (
+    //             <MaterialList
+    //               items={materials}
+    //               onDelete={this.deleteMaterial}
+    //               onUpdate={this.updateMaterial}
+    //             />
+    //         )}
+  }
+}
