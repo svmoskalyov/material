3db3a0a5b78d9a55ff2986b01302621ed6962a59
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-22 15:43:30 +0200

    prop-types for material error

--- src/components/MaterialError/MaterialError.jsx
+++ src/components/MaterialError/MaterialError.jsx
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import { Box } from 'components/Box';
 
 export const MaterialError = ({ message }) => {
   return <Box color="red">{message}</Box>;
+};
+
+MaterialError.propTypes = {
+  message: PropTypes.string.isRequired,
 };
